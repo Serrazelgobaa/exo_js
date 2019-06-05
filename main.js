d83f4d632aa9e2322765cfa40ab31ebb001c7e49
Tania <tania.juspin@gmail.com>
2019-06-05 09:47:49 +0200

    finalisation fonction

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log('Hello World');
+/*console.log('Hello World');
+
+const number1 = 2;
+const number2 = 3;
+
+const additionner = (a, b) => {
+
+	const resultat = a + b;
+
+	return resultat;
+}
+
+const deuxPlusTrois = additionner(number1, number2);
+
+console.log(deuxPlusTrois); */
+
+const reorganiser = (mot) => {
+
+	nouveauMot = mot[0].toUpperCase();
+
+	i = 1;
+
+	while (i < mot.length)
+	{
+		nouveauMot = nouveauMot + mot[i].toLowerCase();
+		
+		i++;
+	}
+	return nouveauMot;
+}
+
+var mot1 = "siMplon";
+var mot2 = "sImplOn";
+var mot3 = "SIMPLON";
+
+console.log(reorganiser(mot1) + reorganiser(mot2) + reorganiser(mot3));
